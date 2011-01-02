82c2f0b1628f00881813e2f5d38ef7cd0d66c88e
mpgarner <m.p.garner@gmail.com>
2011-01-02 17:23:55 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardStack::Application.config.secret_token = '2a1c35459eebe57b4e95af16c4dd6759353acf79150637808723f6e9bc54747e647d9e7aa6ab9b5d61a03afceea6c0be75bc844d81b7f09a0c230ca37729dd10'
---
+++ db/migrate/20110102111501_create_cards.rb
@@ -0,0 +1,15 @@
+class CreateCards < ActiveRecord::Migration
+  def self.up
+    create_table :cards do |t|
+      t.string :name
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :cards
+  end
+end
